4ffc7a34f97851d95f2376b62cd75d49ac0b5b6d
Renato <serra.renato@gmail.com>
2018-11-08 00:10:05 -0200

    ok

--- src/main/java/br/com/next/fsm/enums/App.java
+++ src/main/java/br/com/next/fsm/enums/App.java
@@ -1,22 +1,13 @@
 package br.com.next.fsm.enums;
 
 public class App {
-	private State state;
-
-	public App() {
-	        state = State.INITIAL;
-	    }
-
-	public void performRequest(String aParameter) {
-		state = state.doSomething(aParameter);
-	}
 
 	public static void main(String[] args) {
-		App theObject = new App();
-		theObject.performRequest("Hello");
-		theObject.performRequest("Hello");
-		theObject.performRequest("Hello");
-		theObject.performRequest("Hello");
-		theObject.performRequest("Hello");
+		FSM fsm = new FSM();
+		fsm.next("Hello");
+		fsm.next("Hello");
+		fsm.next("Hello");
+		fsm.next("Hello");
+		fsm.next("Hello");
 	}
 }
